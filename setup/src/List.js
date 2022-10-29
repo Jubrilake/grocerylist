1bf14dfa4b773617ba42df95c9bddaaef69a0261
bayo <jubrilake@gmail.com>
2022-10-29 22:57:45 +0100

    first commit

---
+++ setup/src/List.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FaEdit, FaTrash } from 'react-icons/fa'
+const List = ({items,removeItem,editItem}) => {
+  return <div className='grocery-list'>
+    {items.map(item => {
+      const {id,title} = item
+      return(
+        <article  key={id} className="grocery-item">
+         <p className="title">{title}</p>
+            <button onClick={()=> editItem(id)} type="button" className='edit-btn'>
+              <FaEdit />
+            </button>
+            <button onClick={()=>removeItem(id)} type="button" className='delete-btn'>
+              <FaTrash />
+            </button>
+
+        </article>
+      )
+    })}
+  </div>
+}
+
+export default List
